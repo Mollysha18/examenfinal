435e762c320d5b5e55943ca4c34391ae81a22715
Mollysha18 <luis.susa@hotmail.com>
2018-12-04 08:50:13 -0500

    Esta es mi primera versión de codigo

---
+++ SistemaVentas/src/SistemaVentas.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SistemaVentas.cpp
+// Author      : xx
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "una veneca" << endl; // prints !!!Hello World!!!
+	return 0;
+}
